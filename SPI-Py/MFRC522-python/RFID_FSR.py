088e9d9a1bc84983dcd610b2d184a065963df43d
xinweiiiii <43470166+xinweiiiii@users.noreply.github.com>
2020-10-12 17:50:05 +0800

    Update

--- SPI-Py/MFRC522-python/RFID_FSR.py
+++ SPI-Py/MFRC522-python/RFID_FSR.py
@@ -22,10 +22,9 @@ signal.signal(signal.SIGINT, end_read)
 # Create an object of the class MFRC522
 MIFAREReader = MFRC522.MFRC522()
 
-# GPIO.setmode(GPIO.BCM)
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-client = MongoClient("mongodb+srv://root:0NqePorN2WDm7xYc@cluster0.fvp4p.mongodb.net/iot?retryWrites=true&w=majority")
+db = MongoClient("mongodb+srv://root:0NqePorN2WDm7xYc@cluster0.fvp4p.mongodb.net/iot?retryWrites=true&w=majority")
 
 # Welcome message
 print("RFID Detected!")
@@ -33,27 +32,29 @@ print("FSR Detected")
 
 # This loop keeps checking for chips. If one is near it will get the UID and authenticate
 try:
-        while continue_reading:
-                # Scan for cards
-                (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+    while continue_reading:
+        # Scan for cards
+        (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+        # If a card is found
+        rfid_status = 0 # default for rfid
+        if status == MIFAREReader.MI_OK:
+            print("RFID Tag detected")
+            rfid_status = 1 # rfid tag detected
+        # Get the UID of the card
+        (status,uid) = MIFAREReader.MFRC522_Anticoll()
+        current_time = datetime.now()
+        print("Time is: " + str(current_time))
+        fsr_status = GPIO.input(7)
+        print("Weight sensor: " + str(fsr_status))
 
-                # If a card is found
-                if status == MIFAREReader.MI_OK:
-                        print("Card detected")
-                        
-                        rfid_status = 1
-                        # Get the UID of the card
-                        (status,uid) = MIFAREReader.MFRC522_Anticoll()
-                        current_time = datetime.now()
-                        fsr_status = GPIO.input(7)
-                        if a == 1 and fsr_status == 1:
-                            posts = db.fsr_rfid
-                            post_data = {
-                                'timestamp': current_time,
-                                'rfid_status': rfid_status,
-                                'fsr_status': fsr_status
-                            }
-                            result = posts.insert_one(post_data)
-                        time.sleep(3)   
-except:
-        print("Something went wrong")
+        if rfid_status == 1 and fsr_status == 1:
+            print("RFID and Weight Detected")
+            posts = db.fsr_rfid.collection
+            post_data = {
+                'timestamp': current_time,
+                'rfid_status': rfid_status,
+                'fsr_status': fsr_status
+            }
+            result = posts.insert_one(post_data)
+            print("Inserted with ID: " + str(result))
+        time.sleep(3)
