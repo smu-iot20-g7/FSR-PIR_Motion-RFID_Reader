052f73561bcd712c6ef2c6d291688d4555a66dbc
xinweiiiii <43470166+xinweiiiii@users.noreply.github.com>
2020-10-18 14:36:27 +0800

    Update: Tray out code

--- SPI-Py/MFRC522-python/RFID_FSR.py
+++ SPI-Py/MFRC522-python/RFID_FSR.py
@@ -32,14 +32,13 @@ print("FSR Detected")
 
 # This loop keeps checking for chips. If one is near it will get the UID and authenticate
 try:
+    process_status = True
     while continue_reading:
         # Scan for cards
         (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
         # If a card is found
         rfid_status = 0 # default for rfid
-        if status == MIFAREReader.MI_OK:
-            print("RFID Tag detected")
-            rfid_status = 1 # rfid tag detected
+       
         # Get the UID of the card
         (status,uid) = MIFAREReader.MFRC522_Anticoll()
         current_time = datetime.now()
@@ -47,8 +46,9 @@ try:
         fsr_status = GPIO.input(7)
         print("Weight sensor: " + str(fsr_status))
 
-        if rfid_status == 1 and fsr_status == 1:
-            print("RFID and Weight Detected")
+        if status == MIFAREReader.MI_OK and process_status and fsr_status == 1:
+            print("RFID and Weight Dected")
+            rfid_status = 1 # rfid tag detected
             posts = db.fsr_rfid.collection
             post_data = {
                 'timestamp': current_time,
@@ -57,7 +57,12 @@ try:
             }
             result = posts.insert_one(post_data)
             print("Inserted with ID: " + str(result))
-        time.sleep(3)
+            process_status = False
+
+        if fsr_status == 0:
+            print("Tray not detected")
+            process_status = True
+        time.sleep(2)
 
 except:
     print("Something went wrong")
