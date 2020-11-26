f3e4173cf83fd8e6c876a3875c9abf6de4d69231
xinweiiiii <43470166+xinweiiiii@users.noreply.github.com>
2020-11-26 14:19:34 +0800

    Update script

--- script_trayout.py
+++ script_trayout.py
@@ -6,18 +6,16 @@ from pymongo import MongoClient
 db = MongoClient("mongodb+srv://root:0NqePorN2WDm7xYc@cluster0.fvp4p.mongodb.net/iot?retryWrites=true&w=majority")
 
 # Welcome message
-print("RFID Detected!")
-print("FSR Detected")
+print("RFID - 0")
+print("FSR - 0")
 
 fsr_status = 1
 rfid_status = 1
 process_status = True
 current_time = datetime.now()
 print("Time is: " + str(current_time))
-print("Weight sensor: " + str(fsr_status))
-
-print("RFID and Weight Dected")
-
+print("Force sensor: " + str(fsr_status))
+print("RFID reader: " + str(rfid_status))
 if process_status and fsr_status == 1:
     print("RFID and Weight Dected")
     posts = db.fsr_rfid.collection
